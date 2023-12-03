3a139516a3594278d0a75965b67531abaeb805b0
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-12-04 00:22:01 +0530

    Setup signin and signup pages for the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 import "./App.css";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
