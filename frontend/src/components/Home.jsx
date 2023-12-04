6b51bb00234a40fa0adb907a4b88ee8663a62657
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-12-04 15:05:57 +0530

    Design the home page with CSS

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home-container">
+        <h2 className="home-header">Welcome 👋</h2>
+        <div className="home-subtitle">SignIn or SignUp to get started</div>
+        <div className="home-links">
+          <Link to={"/signup"} className="home-link">
+            SignUp
+          </Link>
+          <Link to={"/signip"} className="home-link">
+            SignIn
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
