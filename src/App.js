1c0fccd1969ad80b8bb41e51c0dfa479161717ce
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-12-06 16:39:38 +0530

    Structure the project, remove frontend directory and move its content to the root directory

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import Customer from "./components/Customer";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/customer" element={<Customer />} />
       </Routes>
     </div>
   );
