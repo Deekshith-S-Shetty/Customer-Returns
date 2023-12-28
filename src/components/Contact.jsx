ace2a399aff25239a0161752e1d256dda891a500
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-12-28 23:29:29 +0530

    Complete the Contact page with animation and design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import "./Contact.css";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-container">
+      <h1 className="contact-header">CONTACT US</h1>
+      <form className="contact-form" action="/">
+        <div className="form-content form-name">
+          <label form="name">Name</label>
+          <input
+            type="text"
+            placeholder="Enter your Name"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+        <div className="form-content form-email">
+          <label form="email">Email</label>
+          <input
+            type="email"
+            placeholder="Enter a valid email address"
+            id="email"
+            name="email"
+            required
+          />
+        </div>
+        <div className="form-content form-message">
+          <label form="desc">Message</label>
+          <textarea
+            placeholder="Enter your Message"
+            rows={7}
+            cols={50}
+            id="desc"
+            name="description"
+            required
+          />
+        </div>
+        <div className="form-content">
+          <button className="submit-btn">Submit Response</button>
+        </div>
+      </form>
+    </div>
+  );
 }
